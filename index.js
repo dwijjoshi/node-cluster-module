796daea3d127397cae0ec48b02a0f14354cc3a11
Your Name <dwijjoshi02@gmail.com>
2024-05-22 13:07:32 +0530

    removed setupPrimary

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import cluster from "cluster";
 import os from "os";
 
-export function nodeCluster(fileName) {
-  const cpuCount = os.cpus().length;
+export function clusterify(app, port) {
+  // Check if the current process is primary or a worker
   if (cluster.isPrimary) {
-    masterProcess();
-  } else {
-  }
+    const numCPUs = os.cpus().length;
 
-  function masterProcess() {
-    if (fileName) {
-      cluster.setupPrimary({
-        exec: `${fileName}`,
-      });
-    }
-    console.log(`Master ${process.pid} is running`);
-    for (let i = 0; i < cpuCount; i++) {
+    // Fork workers equal to the number of CPU cores
+    for (let i = 0; i < numCPUs; i++) {
       cluster.fork();
     }
-    cluster.on("exit", () => {
-      console.log("One worker exited");
+
+    // Handle cluster events
+    cluster.on("exit", (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      // Restarting the worker
       cluster.fork();
+    });
+  } else {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+      console.log(`Process id: ${process.pid}`);
     });
   }
 }
-
-nodeCluster();
